710654fd55382a85ea9d3887a1448d69feb0c22c
yume <785879986@qq.com>
2022-05-01 21:36:50 +0800

    init

---
+++ pages/home/home.js
@@ -0,0 +1,32 @@
+Page({
+	data: {
+		tabs: ['全部服务', '在提供', '正在找'],
+		currentTabIndex: 0,
+		categoryList: [
+			{
+				'id': 1,
+				'name': '保洁'
+			},
+			{
+				'id': 2,
+				'name': '汽修'
+			},
+			{
+				'id': 3,
+				'name': '疏通'
+			},
+		]
+	},
+	onLoad: function (options) {
+	
+	},
+	handleTabChange: function (event) {
+		const index = event.currentTarget.dataset.index
+		this.setData({
+			currentTabIndex: index
+		})
+	},
+	handleCategoryChange: function (event) {
+		const id = event.currentTarget.dataset.id
+	}
+});
