7ec374d39483b97fc742626ddb95bb2a827b4774
yume <785879986@qq.com>
2022-05-02 13:24:47 +0800

    完成手指滑动监听切换标签

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -20,12 +20,31 @@ Component({
 		
 		handleTabChange: function (event) {
 			const index = event.currentTarget.dataset.index
+			if (index === this.data.currentTabIndex) {return}
 			this.setData({
 				currentTabIndex: index
 			})
 			
 			this.triggerEvent('change', {index})
 		},
+		
+		handleTouchMove(event) {
+			const direction = event.direction
+			const currentTabIndex = this.data.currentTabIndex
+			const targetTabIndex = currentTabIndex + direction
+			if (targetTabIndex < 0 || targetTabIndex > this.data.tabs.length - 1) {
+				return
+			}
+			const customEvent = {
+				currentTarget: {
+					dataset: {
+						index: targetTabIndex
+					}
+				}
+			}
+			this.handleTabChange(customEvent)
+		},
+		
 		handleTouchstart: function (event) {
 			console.log(event)
 			//	数据绑定，记录触摸开始的x轴的位置
