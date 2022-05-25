3c84614dbee204ff905c0c9513f9829ed30a6ddf
rin <785879986@qq.com>
2022-05-25 17:10:21 +0800

    增加serviceTypeEnum，对iPhone X以上机型进行安全区域适配

---
+++ enum/service-type.js
@@ -0,0 +1,14 @@
+/**
+ * @name: service-type
+ * @author: Rin
+ * @date: 2022/5/25 16:40
+ * @description：service-type
+ * @update: 2022/5/25 16:40
+ */
+
+const serviceType = {
+	PROVIDE: 1,
+	SEEK: 2
+}
+
+export default serviceType
--- pages/service-detail/service-detail.js
+++ pages/service-detail/service-detail.js
@@ -1,34 +1,40 @@
 import Service from '../../model/service'
 import User from '../../model/user'
 import Rating from '../../model/rating'
+import serviceType from '../../enum/service-type'
 
 const rating = new Rating()
 
 Page({
 	data: {
-		service: null, serviceId: null, isPublisher: false, ratingList: []
-	}, onLoad: async function (options) {
+		service: null,
+		serviceId: null,
+		isPublisher: false,
+		ratingList: [],
+		serviceTypeEnum: serviceType
+	},
+	onLoad: async function (options) {
 		this.data.serviceId = options.service_id
 		await this._getService()
 		await this._getServiceRatingList()
 		this._checkRole()
 	},
-	
-	async _getService() {
+
+	async _getService () {
 		const service = await Service.getServiceByID(this.data.serviceId)
 		this.setData({
 			service
 		})
 	},
-	
-	async _getServiceRatingList() {
+
+	async _getServiceRatingList () {
 		const ratingList = await rating.reset().getServiceRatingList(this.data.serviceId)
 		this.setData({
 			ratingList
 		})
 	},
-	
-	_checkRole() {
+
+	_checkRole () {
 		const userInfo = User.getUserInfoByLocal()
 		if (userInfo && userInfo.id === this.data.service.publisher.id) {
 			this.setData({
