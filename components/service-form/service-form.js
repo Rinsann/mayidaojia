e37fd2487bd4457da9ad7fdf7ef9c4ad5a7732a6
rin <785879986@qq.com>
2022-06-09 23:30:34 +0800

    修复bug

--- components/service-form/service-form.js
+++ components/service-form/service-form.js
@@ -1,5 +1,38 @@
+import serviceType from '../../enum/service-type'
+
 Component({
-	properties: {},
-	data: {},
-	methods: {}
+	properties: {
+		form: Object
+	},
+	data: {
+		typeList: [
+			{
+				id: serviceType.PROVIDE,
+				name: '提供服务'
+			},
+			{
+				id: serviceType.SEEK,
+				name: '找服务'
+			}
+		],
+		typePickerIndex: null
+	},
+	lifetimes: {
+		attached () {
+			this._init()
+		}
+	},
+	methods: {
+		_init () {
+			//	ES6
+			// 找不到的时候，返回-1
+			const index = this.data.typeList.findIndex(item => this.form.type === item.id)
+			this.setData({
+				typePickerIndex: index !== -1 ? index : null
+			})
+		},
+		handleTypeChange: function (event) {
+			console.log(event)
+		}
+	}
 });
